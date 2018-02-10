95a1c9866880a1128ad544b1e1a4e73829cc799d
Karrotts <WesleyMiller1996@gmail.com>
2018-02-10 09:58:43 -0600

    Changed bullet physics to be less lame

--- Source/Project_Depth/Project_DepthProjectile.cpp
+++ Source/Project_Depth/Project_DepthProjectile.cpp
@@ -8,7 +8,7 @@ AProject_DepthProjectile::AProject_DepthProjectile()
 {
 	// Use a sphere as a simple collision representation
 	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
-	CollisionComp->InitSphereRadius(5.0f);
+	CollisionComp->InitSphereRadius(50.0f);
 	CollisionComp->BodyInstance.SetCollisionProfileName("Projectile");
 	CollisionComp->OnComponentHit.AddDynamic(this, &AProject_DepthProjectile::OnHit);		// set up a notification for when this component hits something blocking
 
@@ -22,13 +22,13 @@ AProject_DepthProjectile::AProject_DepthProjectile()
 	// Use a ProjectileMovementComponent to govern this projectile's movement
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
 	ProjectileMovement->UpdatedComponent = CollisionComp;
-	ProjectileMovement->InitialSpeed = 3000.f;
-	ProjectileMovement->MaxSpeed = 3000.f;
+	ProjectileMovement->InitialSpeed = 30000.f;
+	ProjectileMovement->MaxSpeed = 30000.f;
 	ProjectileMovement->bRotationFollowsVelocity = true;
 	ProjectileMovement->bShouldBounce = true;
 
 	// Die after 3 seconds by default
-	InitialLifeSpan = 3.0f;
+	InitialLifeSpan = 1.0f;
 }
 
 void AProject_DepthProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
